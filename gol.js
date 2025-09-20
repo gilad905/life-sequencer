e719d4cbeae9fb68b53d16dffc09232ce494fd90
gilad <gilad905@gmail.com>
2025-09-21 01:46:47 +0200

    game of life added

--- gol.js
+++ gol.js
@@ -1,6 +1,46 @@
+(function () {
+  new Tone.Loop(onTick, "2n").start("2n");
+  const sequencer = document.querySelector("tone-step-sequencer");
 
+  function onTick() {
+    const matrix = sequencer._matrix.map((row) => row.slice());
 
-function step() {}
+    for (let y = 0; y < matrix.length; y++) {
+      for (let x = 0; x < matrix[y].length; x++) {
+        const neighbors = countNeighbors(x, y);
+        // console.log(x, y, neighbors);
+        if (matrix[y][x]) {
+          // Cell is alive
+          matrix[y][x] = neighbors === 2 || neighbors === 3;
+        } else {
+          // Cell is dead
+          matrix[y][x] = neighbors === 3;
+        }
+      }
+    }
 
-window.ls ??= {};
-window.ls.gol = { step };
+    sequencer._matrix = matrix;
+    sequencer.requestUpdate();
+  }
+
+  function countNeighbors(x, y) {
+    const { _matrix } = sequencer;
+    let count = 0;
+    for (let cY = y - 1; cY <= y + 1; cY++) {
+      for (let cX = x - 1; cX <= x + 1; cX++) {
+        if (cY === y && cX === x) continue; // Skip the cell itself
+        if (
+          cY >= 0 &&
+          cY < _matrix.length &&
+          cX >= 0 &&
+          cX < _matrix[0].length
+        ) {
+          if (_matrix[cY][cX]) {
+            count++;
+          }
+        }
+      }
+    }
+    return count;
+  }
+})();
--- main.js
+++ main.js
@@ -1,48 +1,44 @@
-const toggler = document.querySelector("tone-play-toggle");
-toggler.addEventListener("start", () => Tone.Transport.start());
-toggler.addEventListener("stop", () => Tone.Transport.stop());
+(function () {
+  const toggler = document.querySelector("tone-play-toggle");
+  toggler.addEventListener("start", () => Tone.Transport.start());
+  toggler.addEventListener("stop", () => Tone.Transport.stop());
 
-const slider = document.querySelector("tone-slider");
-slider.addEventListener(
-  "input",
-  (e) => (Tone.Transport.bpm.value = parseFloat(e.target.value))
-);
+  const slider = document.querySelector("tone-slider");
+  slider.addEventListener(
+    "input",
+    (e) => (Tone.Transport.bpm.value = parseFloat(e.target.value))
+  );
 
-const synthKeys = ["A3", "Db4", "E4", "Gb4"];
+  const synthKeys = ["A3", "Db4", "E4", "Gb4"];
 
-const loop = new Tone.Loop(onTick, "8n").start(0);
-
-const sequencer = document.querySelector("tone-step-sequencer");
-for (let i = 0; i < sequencer.rows; i++) {
-  sequencer._matrix[i][i] = true;
-}
-sequencer.requestUpdate();
-
-const synths = [
-  new Tone.PolySynth(Tone.Synth, { oscillator: { type: "sine" } }),
-  new Tone.Players({
-    baseUrl: "https://tonejs.github.io/audio/drum-samples/4OP-FM/",
-    urls: { 0: "hihat.mp3", 1: "kick.mp3", 2: "snare.mp3", 3: "tom1.mp3" },
-    fadeOut: "64n",
-  }),
-  new Tone.PolySynth(Tone.Synth, { oscillator: { type: "sawtooth" } }),
-];
-
-for (const synth of synths) {
-  synth.toDestination();
-}
-
-sequencer.addEventListener("trigger", ({ detail }) => {
-  const synthI = parseInt((detail.row / sequencer.rows) * 3);
-  const synth = synths[synthI];
-  const synthRow = detail.row % (sequencer.rows / 3);
-  if (synthI == 1) {
-    synth.player(synthRow).start(detail.time, 0, "16t");
-  } else {
-    synth.triggerAttackRelease(synthKeys[synthRow], "16t", detail.time);
+  const sequencer = document.querySelector("tone-step-sequencer");
+  for (let i = 0; i < sequencer.rows; i++) {
+    sequencer._matrix[i][i] = true;
   }
-});
+  sequencer.requestUpdate();
 
-function onTick(time) {
-  window.ls.gol.step();
-}
+  const synths = [
+    new Tone.PolySynth(Tone.Synth, { oscillator: { type: "sine" } }),
+    new Tone.Players({
+      baseUrl: "https://tonejs.github.io/audio/drum-samples/4OP-FM/",
+      urls: { 0: "hihat.mp3", 1: "kick.mp3", 2: "snare.mp3", 3: "tom1.mp3" },
+      fadeOut: "64n",
+    }),
+    new Tone.PolySynth(Tone.Synth, { oscillator: { type: "sawtooth" } }),
+  ];
+
+  for (const synth of synths) {
+    synth.toDestination();
+  }
+
+  sequencer.addEventListener("trigger", ({ detail }) => {
+    const synthI = parseInt((detail.row / sequencer.rows) * 3);
+    const synth = synths[synthI];
+    const synthRow = detail.row % (sequencer.rows / 3);
+    if (synthI == 1) {
+      synth.player(synthRow).start(detail.time, 0, "16t");
+    } else {
+      synth.triggerAttackRelease(synthKeys[synthRow], "16t", detail.time);
+    }
+  });
+})();
